ba2b4ae8be1faed66bd193f5e8c848d4c3dec61d
hao-nhien.nguyen <hao-nhien.nguyen@epitech.eu>
2020-06-03 19:17:41 +0200

    api get all products

--- server/src/Entity/Product.php
+++ server/src/Entity/Product.php
@@ -5,6 +5,9 @@ namespace App\Entity;
 use App\Repository\ProductRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
+use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Validator\Mapping\ClassMetadata;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -13,56 +16,67 @@ use Doctrine\ORM\Mapping as ORM;
 class Product
 {
     /**
+     * @Groups("products")
      * @ORM\Id()
      * @ORM\GeneratedValue()
      * @ORM\Column(type="integer")
+     * @Groups("products")
      */
     private $id;
 
     /**
      * @ORM\OneToOne(targetEntity=Category::class, inversedBy="product", cascade={"persist", "remove"})
      * @ORM\JoinColumn(nullable=false)
+     * @Groups("products")
      */
     private $Category;
 
     
     /**
      * @ORM\Column(type="string", length=255)
+     * @Groups("products")
      */
     private $title;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Groups("products")
      */
     private $description;
 
     /**
      * @ORM\Column(type="float",nullable=true)
+     * @Groups("products")
      */
     private $price;
 
     /**
      * @ORM\Column(type="integer", nullable=true)
+     * @Groups("products")
      */
     private $promo;
 
     /**
      * @ORM\Column(type="datetime")
+     * @Groups("products")
      */
     private $created_at;
 
     /**
      * @ORM\Column(type="integer", nullable=true)
+     * @Groups("products")
      */
     private $clicks;
 
     /**
-     * @ORM\OneToMany(targetEntity=Subproduct::class, mappedBy="product")
+     * @ORM\OneToMany(targetEntity=Subproduct::class, mappedBy="product", fetch="EAGER")
+     * @Groups("products")
      */
     private $subproducts;
 
     /**
      * @ORM\Column(type="string", length=255, nullable=true)
+     * @Groups("products")
      */
     private $sex;
 
@@ -71,6 +85,14 @@ class Product
     public function __construct()
     {
         $this->subproducts = new ArrayCollection();
+    }
+
+    public static function loadValidatorMetadata(ClassMetadata $metadata)
+    {
+        $metadata->addPropertyConstraint('title', new Assert\NotBlank());
+        $metadata->addPropertyConstraint('Category', new Assert\NotBlank());
+        $metadata->addPropertyConstraint('description', new Assert\NotBlank());
+        $metadata->addPropertyConstraint('price', new Assert\NotBlank());
     }
 
     public function getId(): ?int
@@ -114,32 +136,6 @@ class Product
 
         return $this;
     }
-
-    public function getStock(): ?int
-    {
-        return $this->stock;
-    }
-
-    public function setStock(int $stock): self
-    {
-        $this->stock = $stock;
-
-        return $this;
-    }
-
-    public function getSize(): ?string
-    {
-        return $this->size;
-    }
-
-    public function setSize(string $size): self
-    {
-        $this->size = $size;
-
-        return $this;
-    }
-
-
 
     public function getSex(): ?string
     {
