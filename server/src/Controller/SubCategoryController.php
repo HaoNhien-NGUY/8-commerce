d9469de47b37cc7fd183067b6e4ec28048f9e2af
Nordine <nordine.el-barkaoui@epitech.eu>
2020-06-05 16:13:31 +0200

    crud category sub category

--- server/src/Controller/SubCategoryController.php
+++ server/src/Controller/SubCategoryController.php
@@ -87,4 +87,38 @@ class SubCategoryController extends AbstractController
         }
     }
 
+    /**
+     * @Route("/api/subcategory/{id}", name="subcategory_update", methods="PUT")
+     */
+    public function subcategoryUpdate(Request $request, EntityManagerInterface $em, ValidatorInterface $validator, CategoryRepository $categoryRepository,SubCategoryRepository $subCategoryRepository)
+    {
+        try {
+            $jsonContent = $request->getContent();
+            $req = json_decode($jsonContent);
+            $subCategory = $subCategoryRepository->findOneBy(['id' => $request->attributes->get('id')]);
+            
+            if ($subCategory) {
+                if (isset($req->name)) {
+                    $subCategory->setName($req->name);
+                }
+                if(isset($req->category)){
+                    $category = $categoryRepository->findOneBy(['id' => $req->category]);
+                    $subCategory->setCategory($category);
+                }
+
+                $error = $validator->validate($subCategory);
+                if (count($error) > 0) return $this->json($error, 400);
+
+                $em->persist($subCategory);
+                $em->flush();
+
+                return $this->json($subCategory, 200, [], ['groups' => 'category']);
+            } else {
+                return $this->json(['message' => 'category not found'], 404, []);
+            }
+        } catch (NotEncodableValueException $e) {
+            return $this->json($e->getMessage(), 400);
+        }
+    }
+
 }
