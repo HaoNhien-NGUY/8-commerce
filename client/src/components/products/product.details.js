9e1eb9ebd0192cafa13ddaa98d7eb8d4277f0142
Victor Reyes <victor.reyes@hotmail.fr>
2020-06-05 02:17:07 +0200

    Debug Ternaire ==> Choice Color | Size on /product/id

--- client/src/components/products/product.details.js
+++ client/src/components/products/product.details.js
@@ -21,7 +21,6 @@ function ProductDescription() {
         }
     }, []);
 
-    console.log(product)
     const [completeDes, setCompleteDes] = useState('.. More ⇒');
     const imageProduct = [];
     const miniImageProduct = [];
@@ -112,12 +111,22 @@ function ProductDescription() {
     }
 
     const verifyIfAProductIsChosen = () => {
+        console.log('verifyIfAProductIsChosen')
         console.log(chosenSubProduct)
-        if (chosenSubProduct && chosenSubProduct != null && Object.keys(chosenSubProduct).length > 0) {
+        if (chosenSubProduct && chosenSubProduct != null) {
             return true;
         } else {
             return false;
         }
+    }
+
+    const createOptions = () => {
+        let arr = product.subproducts.filter(item => item.size == chosenProductSize)
+        let options = []
+        for (let i = 0; i < arr.length; i++) {
+            options.push(<option key={arr[i].id} value={arr[i].color} className='selectChoice'>{arr[i].color}</option>)
+        }
+        return options
     }
 
     return (
@@ -137,34 +146,28 @@ function ProductDescription() {
                         <div className='divDetails'>
                             <span>Path/to/category/article</span>
                             <h1>{details.title}</h1>
-                            <h3 className='prix'>{verifyIfAProductIsChosen() ? chosenSubProduct.price : details.price} €</h3> 
-                            {/* le code pour afficher le produit choisit  {verifyIfAProductIsChosen() ? chosenSubProduct.price : details.price}   */}
+                            <h3 className='prix'>{verifyIfAProductIsChosen() ? chosenSubProduct.price + '€': console.log(details.price)} </h3>
                             <p className='description'>
                                 {details.description_1}
                                 <span className='complete'>{details.description_2}</span>
                             </p>
                             <p className="more" onClick={Complete}>{completeDes}</p>
-                            <p>il reste {verifyIfAProductIsChosen() ? chosenSubProduct.stock : countstockAll} pièces disponibles</p>
-                         {chosenProductSize != '' && chosenProductColor != '' ? setChosenProduct() : ''}
-                          
-                            <select id='selectSize' onChange={e => e.preventDefault() + setChosenProductSize(e.target.value) + console.log('size')}>
-                                <option value='' className='selectChoice'>--- SIZE ---</option>
+                            <p>{verifyIfAProductIsChosen() ? chosenSubProduct.stock + " pièces disponibles" : countstockAll + " pièces totales disponibles"}</p>
+                            
+                            {chosenProductSize != '' && chosenProductColor != '' ? console.log('PRODUCT IS CHOSEN') + setChosenProduct() : console.log('NO CHOICE')}
+
+                            <select id='selectSize' onChange={e => e.preventDefault() + console.log(e.target.value) + setChosenProductSize(e.target.value)}>
+                                <option value='' className='selectChoice' >--- SIZE ({SizeOption.length})---</option>
                                 {SizeOption}
-                                {/* <option value='S'>S</option>
-                                <option value='M'>M</option>
-                                <option value='L'>L</option>
-                                <option value='XL'>XL</option> */}
                             </select>
 
-                            <select id='selectColor' onChange={e => e.preventDefault() + setChosenProductColor(e.target.value) + console.log('color')}>
-                                <option value='' className='selectChoice'>--- COLOR ---</option>
-                                {ColorOption}
-
-                                {/* <option value='black'>Black</option>
-                                <option value='white'>White</option>
-                                <option value='grey'>Grey</option>
-                                <option value='khaki'>Khaki</option> */}
-                            </select>
+                            {chosenProductSize ?
+                                <select id='selectColor' onChange={e => e.preventDefault() + setChosenProductColor(e.target.value)}>
+                                    <option value='No-Choice-Color' key="001" className='selectChoice'>--- COLOR ({createOptions().length}) ---</option>
+                                    {createOptions()}
+                                </select>
+                                : null
+                            }
 
                             <button className='btn-cart'>Add to cart</button>
                         </div>
