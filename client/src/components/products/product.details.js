71aa98ecf069eabeba6ee1ea2abae0ac83a5aa75
Audrey Claris <audrey claris>
2020-06-05 19:36:38 +0200

    Adding new product page responsive

--- client/src/components/products/product.details.js
+++ client/src/components/products/product.details.js
@@ -67,7 +67,7 @@ function ProductDescription() {
                 block: 'start',
             });
         imageProduct.push(<span ref={ref}><ReactImageMagnify {...showImage(value)} /></span>);
-        miniImageProduct.push(<li onClick={handleClick}><img className='imgIcone' src={value} /></li>);
+        miniImageProduct.push(<div onClick={handleClick}><img className='imgIcone' src={value} /></div>);
     }
 
     function showImage(pathImg) {
@@ -125,54 +125,101 @@ function ProductDescription() {
     }
 
     return (
-        <div id="maincontainer" className="container-fluid m-0 p-0">
-            {loadingScreen}
-            <div className=''>
-                <div className='divProduct'>
-                    <div className='col-md-7 divImages'>
+<div>
+
+<div class="container-fluid m-0 p-0">
+    <div class="row">
+        <div class="col-md-7 productImgBg m-0 p-0">
+        <div className='ulImgProduct'>
+        {miniImageProduct}
+        </div>
+        <div className='ImageContainer'>
+        {imageProduct}
+        </div>
+        </div>
+        <div class="col-md-5 m-0 p-0">
+        <div class="divDetails">
+        <span>Path/to/category/article</span>
+        <h1>{details.title}</h1>
+        <h3 className='prix'>{verifyIfAProductIsChosen() ? chosenSubProduct.price + '€': console.log(details.price)} </h3>
+        <p className='description'>
+        {details.description_1}
+        <span className='complete'>{details.description_2}</span>
+        </p>
+        <p className="more" onClick={Complete}>{completeDes}</p>
+        <p>{verifyIfAProductIsChosen() ? chosenSubProduct.stock + " pièces disponibles" : countstockAll + " pièces totales disponibles"}</p>
+        {chosenProductSize != '' && chosenProductColor != '' ? console.log('PRODUCT IS CHOSEN') + setChosenProduct() : console.log('NO CHOICE')}
+        <select id='selectSize' onChange={e => e.preventDefault() + console.log(e.target.value) + setChosenProductSize(e.target.value)}>
+        <option value='' className='selectChoice' >--- SIZE ({SizeOption.length})---</option>
+        {SizeOption}
+        </select>
+        {chosenProductSize ?
+        <select id='selectColor' onChange={e => e.preventDefault() + setChosenProductColor(e.target.value)}>
+        <option value='No-Choice-Color' key="001" className='selectChoice'>--- COLOR ({createOptions().length}) ---</option>
+        {createOptions()}
+        </select>
+        : null
+        }
+        <button className='btn-cart'>Add to cart</button>
+        </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-sm-12 sndrow"><br /> <br /> <br /> <br /> <br /> <br /> <br />   <br /> <br /> <br /> <br /> <br /> <br /> <br />   <br /> <br /> <br /> <br /> <br /> <br /> <br /></div>
+    </div>
+</div>
+
+                        {/* <div id="maincontainer" className="container-fluid m-0 p-0">
+                        {loadingScreen}
+                        <div className=''>
+                        <div className='divProduct'>
+                        <div className='col-md-7 divImages'>
                         <ul className='ulImgProduct'>
-                            {miniImageProduct}
+                        {miniImageProduct}
                         </ul>
                         <div className=''>
-                            {imageProduct}
+                        {imageProduct}
                         </div>
-                    </div>
-                    <div className='col-md-5 bg-white'>
+                        </div>
+                        <div className='col-md-5 bg-white'>
                         <div className='divDetails'>
-                            <span>Path/to/category/article</span>
-                            <h1>{details.title}</h1>
-                            <h3 className='prix'>{verifyIfAProductIsChosen() ? chosenSubProduct.price + '€': console.log(details.price)} </h3>
-                            <p className='description'>
-                                {details.description_1}
-                                <span className='complete'>{details.description_2}</span>
-                            </p>
-                            <p className="more" onClick={Complete}>{completeDes}</p>
-                            <p>{verifyIfAProductIsChosen() ? chosenSubProduct.stock + " pièces disponibles" : countstockAll + " pièces totales disponibles"}</p>
-                            
-                            {chosenProductSize != '' && chosenProductColor != '' ? console.log('PRODUCT IS CHOSEN') + setChosenProduct() : console.log('NO CHOICE')}
+                        <span>Path/to/category/article</span>
+                        <h1>{details.title}</h1>
+                        <h3 className='prix'>{verifyIfAProductIsChosen() ? chosenSubProduct.price + '€': console.log(details.price)} </h3>
+                        <p className='description'>
+                        {details.description_1}
+                        <span className='complete'>{details.description_2}</span>
+                        </p>
+                        <p className="more" onClick={Complete}>{completeDes}</p>
+                        <p>{verifyIfAProductIsChosen() ? chosenSubProduct.stock + " pièces disponibles" : countstockAll + " pièces totales disponibles"}</p>
 
-                            <select id='selectSize' onChange={e => e.preventDefault() + console.log(e.target.value) + setChosenProductSize(e.target.value)}>
-                                <option value='' className='selectChoice' >--- SIZE ({SizeOption.length})---</option>
-                                {SizeOption}
-                            </select>
+                        {chosenProductSize != '' && chosenProductColor != '' ? console.log('PRODUCT IS CHOSEN') + setChosenProduct() : console.log('NO CHOICE')}
 
-                            {chosenProductSize ?
-                                <select id='selectColor' onChange={e => e.preventDefault() + setChosenProductColor(e.target.value)}>
-                                    <option value='No-Choice-Color' key="001" className='selectChoice'>--- COLOR ({createOptions().length}) ---</option>
-                                    {createOptions()}
-                                </select>
-                                : null
-                            }
-                            <button className='btn-cart'>Add to cart</button>
+                        <select id='selectSize' onChange={e => e.preventDefault() + console.log(e.target.value) + setChosenProductSize(e.target.value)}>
+                        <option value='' className='selectChoice' >--- SIZE ({SizeOption.length})---</option>
+                        {SizeOption}
+                        </select>
+
+                        {chosenProductSize ?
+                        <select id='selectColor' onChange={e => e.preventDefault() + setChosenProductColor(e.target.value)}>
+                        <option value='No-Choice-Color' key="001" className='selectChoice'>--- COLOR ({createOptions().length}) ---</option>
+                        {createOptions()}
+                        </select>
+                        : null
+                        }
+                        <button className='btn-cart'>Add to cart</button>
                         </div>
-                    </div>
-                </div>
-            </div>
+                        </div>
+                        </div>
+                        </div>
 
-            <div>
-                <br /> <br /> <br /> <br /> <br /> <br /> <br />   <br /> <br /> <br /> <br /> <br /> <br /> <br />   <br /> <br /> <br /> <br /> <br /> <br /> <br />
-            </div>
-        </div>
+                        <div>
+                        <br /> <br /> <br /> <br /> <br /> <br /> <br />   <br /> <br /> <br /> <br /> <br /> <br /> <br />   <br /> <br /> <br /> <br /> <br /> <br /> <br />
+                        </div>
+                        </div> */}
+</div>
+
+
     )
 }
 
