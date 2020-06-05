6c4b3b3b158161d3e29f7fd1bbfa90da593497ab
Antonio Tina <vskoolvs@gmail.com>
2020-06-05 11:04:18 +0200

    Added pagination in admininterface.js. Fixed showing 403 bug

--- client/src/components/Admin/CreateCategorySub/AccessCreateSubCategory.js
+++ client/src/components/Admin/CreateCategorySub/AccessCreateSubCategory.js
@@ -1,43 +1,46 @@
 import React, { useEffect } from 'react';
 import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link
-  } from "react-router-dom";
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import { connect } from 'react-redux'
 import { Redirect } from 'react-router-dom';
 import PropTypes from "prop-types";
+import { Spinner } from 'react-bootstrap'
 import CreateSubCategory from './CreateSubCategory';
 
-const AccessCreateSubCategory = ({auth}) => {
-  
-  if(!auth.authenticated)
-  {
-    if(auth.user !== null && auth.user.role === 'admin')
-    {
-        return(
-            <CreateSubCategory />
-        )
+const AccessCreateSubCategory = ({ auth }) => {
+
+  if (!auth.authenticated && !auth.isLoading) {
+    if (auth.user !== null && auth.user.role === 'admin') {
+      return (
+        <CreateSubCategory />
+      )
     }
-    else
-    {
-       return(<div id='error403'> <h2> Error page 403 access forbiden </h2></div>)
+    else {
+      return (<div id='error403'> <h2> Error page 403 access forbiden </h2></div>)
     }
   }
-  else
-  {
-      return(<div>Loading...</div>)
+  else {
+    return (
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <div>
+          <Spinner style={{ top: '33%', margin: '0', position: 'absolute' }} className="" animation="grow" />
+        </div>
+      </div>
+    )
   }
 
 }
 
 AccessCreateSubCategory.propTypes = {
-    auth: PropTypes.object.isRequired
-  };
+  auth: PropTypes.object.isRequired
+};
 
 const mapStateToProps = state => ({
-    auth: state.auth,
+  auth: state.auth,
 })
 
 export default connect(mapStateToProps)(AccessCreateSubCategory)
--- client/src/components/Admin/UpdateProduct/AccessUpdateProduct.js
+++ client/src/components/Admin/UpdateProduct/AccessUpdateProduct.js
@@ -9,12 +9,12 @@ import { connect } from 'react-redux'
 import { Redirect } from 'react-router-dom';
 import PropTypes from "prop-types";
 import './AccessUpdateProduct.css';
+import { Spinner } from 'react-bootstrap'
 import UpdateProduct from './UpdateProduct';
 
 const AccessUpdateProduct = ({auth}) => {
   
-  if(!auth.authenticated)
-  {
+  if (!auth.authenticated && !auth.isLoading) {
     if(auth.user !== null && auth.user.role === 'admin')
     {
         return(
@@ -28,7 +28,13 @@ const AccessUpdateProduct = ({auth}) => {
   }
   else
   {
-      return(<div>Loading...</div>)
+    return (
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <div>
+          <Spinner style={{ top: '33%', margin: '0', position: 'absolute' }} className="" animation="grow" />
+        </div>
+      </div>
+    )
   }
 
 }
