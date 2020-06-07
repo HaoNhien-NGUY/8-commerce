5f19f7be2ad3b26bdc1387a0c61ff7f9a486bdec
hao-nhien.nguyen <hao-nhien.nguyen@epitech.eu>
2020-06-07 02:32:39 +0200

    API fixed image path and modified home.js

--- client/src/components/home/home.js
+++ client/src/components/home/home.js
@@ -14,23 +14,53 @@ function Home() {
     var imageProduit1 = '';
     var imageProduit2 = ''
 
+    const isEmpty = (obj) => {
+        for (var key in obj) {
+            if (obj.hasOwnProperty(key))
+                return false;
+        }
+        return true;
+    }
+
     useEffect(() => {
-        axios.get('http://127.0.0.1:8000/api/product').then(resp => {            
+        axios.get('http://127.0.0.1:8000/api/product').then(resp => {
+
+
             //Display Lowest Price Image
-            let arr = []
+            let prices = {}
             let products_temp = resp.data.data
-            console.log(products_temp.subproducts);
-            if(products_temp.subproducts && products_temp.subproducts.length > 0)
-            {   
-                $.each(products_temp, (index, product) => {
-                    product.subproducts.map(item => arr.push(item.price))
-                });
-                for (let i = 0; i < products_temp.length; i++) {
-                    products_temp[i]['lowest_price'] = arr[i];
+            let unavailable_msg = 'Available Soon...'
+
+            $.each(products_temp, (i, product) => {
+                prices[product.id] = []
+                if (isEmpty(product.subproducts)) {
+                    //Product doesnt have subproducts, display unavailable_msg
+                    prices[product.id].push(unavailable_msg)
+                } else {
+                    //Product has subproducts
+                    product.subproducts.map(p => prices[product.id].push(p.price))
                 }
-                setProducts(products_temp)
+            })
+
+            //Boucler sur l'objet pour voir si il y a un prix minimal ou si il n'est pas disponible
+            if (Object.keys(prices).length > 0) {
+                const entries = Object.entries(prices)
+                console.log(entries)
+                for (const [id, prices_list] of entries) {
+                    for (let j = 0; j < products_temp.length; j++) {
+                        
+                        if ( products_temp[j].id == id ){
+                            if ( prices_list[0] == unavailable_msg ) {
+                                products_temp[j]['lowest_price'] = unavailable_msg
+                            } else {
+                                products_temp[j]['lowest_price'] = 'Starts at '+Math.min.apply(Math, prices_list)+' €'
+                            }
+                        }
+                        
+                    }
+                }
             }
-            
+            setProducts(products_temp)
         });
         return () => {
         }
@@ -49,7 +79,7 @@ function Home() {
 
                 {products.map((e) => {
 
-                    console.log(e)
+                    // console.log(e)
 
                     //     if(e.subproducts[0] && e.subproducts[0])
                     //     {
@@ -72,10 +102,10 @@ function Home() {
                                 {/* <img src={'../../../../images/1/default/2020-06-0603-16-51.jpg'} />  */}
                                 <div className='p-4 m-5 bg-gray'>
                                     <span className="HomeArticleTItle">{e.title}</span>
-                                    <p>Starts at {e.lowest_price} €</p>
+                                    <p>{e.lowest_price}</p>
                                     <a href={"/product/" + e.id}>
                                         <div className="ProductHomeImgContainer">
-                                            <img className="ProductHomeImg" src={imageProduit1}></img>
+                                            <img className="ProductHomeImg" src={e.images !== undefined ? 'http://127.0.0.1:8000/api'+e.images[0] : imageProduit1}></img>
                                             <img className="ProductHomeImg ProductHomeImg2" src={imageProduit2}></img>
                                         </div>
                                     </a>
