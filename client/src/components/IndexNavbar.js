912a8bbe4a350b86857dae7791b33134e13a96f5
Audrey Claris <audrey claris>
2020-06-10 15:48:57 +0200

    Full panier editing

--- client/src/components/IndexNavbar.js
+++ client/src/components/IndexNavbar.js
@@ -34,7 +34,6 @@ class IndexNavbar extends Component {
   };
 
   componentDidMount() {
-    const Message = [];
     let panier = JSON.parse(sessionStorage.getItem("panier", []));
     if (panier) {
       panier.map((e) => {
@@ -175,9 +174,9 @@ class IndexNavbar extends Component {
                           </tr>
                           <tr className="tableborder">
                             <td className="detailsproduct">
-                              <span>color: {e.color.name}</span>
+                              <span>quantity: {e.quantity}</span>
                               <span>size: {e.size}</span>
-                              <span>quantity:s {e.quantity}</span>
+                              <span>price: {e.price} €</span>
                             </td>
                           </tr>
                         </>
