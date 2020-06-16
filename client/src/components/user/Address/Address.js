21db97586c233bff04787b79ac0879b2b1185db0
hakim-nemar <hakim.nemar@epitech.eu>
2020-06-16 17:19:43 +0200

    Preparation component suer

---
+++ client/src/components/user/Address/Address.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from "react";
+
+function Address() {
+    return (
+        <>
+            <h1>Address</h1>
+        </>
+    )
+}
+
+export default Address;
---
+++ client/src/components/user/History/History.js
@@ -0,0 +1,23 @@
+import React, { useState, useEffect } from "react";
+
+function ShowOrder() {
+    // axios.get("http://localhost:8000/api/user/" + props.idUser + "/orders").then(res => {
+    //         console.log(res.data.userOrders);
+    //         const newDataHistory = res.data.userOrders.map(e => 
+    //                 <tr>
+
+    //                 </tr>
+    //             )
+    //         setHistory(newDataHistory);
+    //     }).catch(err => {
+    //         console.log(err);
+    //     });
+    
+    return (
+        <>
+            <h1>History</h1>
+        </>
+    )
+}
+
+export default ShowOrder;
