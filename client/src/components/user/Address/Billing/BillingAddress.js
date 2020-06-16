e5161f5d36357ff84f2aaf838dbcf8d5e61dc211
hakim-nemar <hakim.nemar@epitech.eu>
2020-06-16 17:36:48 +0200

    discint shipping/billing

---
+++ client/src/components/user/Address/Billing/BillingAddress.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from "react";
+
+function BillingAddress() {
+    return (
+        <>
+            <h1>BillingAddress</h1>
+        </>
+    )
+}
+
+export default BillingAddress;
