8f2b01c58940066be983d738df90f713f9cb6bc1
Audrey Claris <audrey claris>
2020-06-25 02:00:57 +0200

    New Homepage till last jumbo

--- client/src/components/home/sliderPromo.js
+++ client/src/components/home/sliderPromo.js
@@ -23,37 +23,38 @@ export default function SliderPromo() {
 
         console.log(data);
       })
-  }, [isDataReady])  
+  }, [isDataReady])
 
   let imageDefault = "https://i.ibb.co/j5qSV4j/missing.jpg";
 
   const SliderData = data.map(product => {
-    return (<div className="slider-bg text-center m-auto row m-0 p-0" key={product.id}>
-      <div className="col-2"></div>
-      <div className="col-4">
-        <img src={product.images && product.images[0] ? process.env.REACT_APP_API_LINK + product.images[0]  : imageDefault} className="img-div p-3"/>
+    return (<div className="row slider-bg d-flex  align-items-center" key={product.id}>
+      <div className="m-0 p-0 col-2 "></div>
+      <div className="m-0 mt-5 p-0 col-5 h-100" >
+        <img src={product.images && product.images[0] ? process.env.REACT_APP_API_LINK + product.images[0] : imageDefault} className="img-div" />
       </div>
-      <div className="col-4 text-left">
+      <div className="col-3 text-left h-50">
         <h1 className="">{product.title}</h1>
         <p>{product.description}</p>
-        <a href={"/product/" + product.id}><button className='btn-cart'>View Product</button></a>
+        <a href={"/product/" + product.id}><button className='w-50 btn-cart'>View Product</button></a>
       </div>
-      <div className="col-2 p-0"></div>
+      <div className="m-0  col-2 p-0 "></div>
     </div>
-  )})
+    )
+  })
   const AutoplaySlider = withAutoplay(AwesomeSlider);
 
   return (
-    <div className='container-fluid' id="slider">
-        {data.length > 0 && 
-          <AutoplaySlider
-            play={true}
-            cancelOnInteraction={false} // should stop playing on user interaction
-            interval={6000}
-          >
-            {SliderData}
-          </AutoplaySlider>
-        }
+    <div className='container-fluid m-0 p-0 pt-2' id="slider">
+      {data.length > 0 &&
+        <AutoplaySlider
+          play={true}
+          cancelOnInteraction={false} // should stop playing on user interaction
+          interval={6000}
+        >
+          {SliderData}
+        </AutoplaySlider>
+      }
     </div>
   )
 }
